5bbeb2cd84ab953d0e58cfd1e13c859a62efaa94
huangyaru <huangyaru@emay.cn>
2019-09-10 14:50:50 +0800

    日志管理添加导入导出功能

--- src/api/system/syslog.js
+++ src/api/system/syslog.js
@@ -1,4 +1,5 @@
 import request from '@/utils/HttpUtils'
+import requestUpload from '@/utils/UploadHttpUtils'
 // 系统日志请求
 export function syslog(data) {
   return request({
@@ -7,3 +8,12 @@ export function syslog(data) {
     data: data
   })
 }
+// 导入
+export function importMehtod(data) {
+  return requestUpload({
+    url: '/userlog/import',
+    method: 'post',
+    data: data
+  })
+}
+
